0da70328f9bafa107865107a25b0d834e5d7acd8
jlalonde-code <jlalonde@umass.edu>
2025-10-21 12:56:19 -0400

    Set up Express server with a Hello world route

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('Hello world');
+const express = require('express');
+const app = express();
+
+// Use the port Render provides
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
